5dad9f1bdc16ed057c122b08e6a99536371aba19
Bryce Callender <brycec848@gmail.com>
2022-06-27 20:57:17 -0700

    hand wip

--- MapleDailyTracker/main.cpp
+++ MapleDailyTracker/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setOverrideCursor(QCursor(QPixmap(":/cursor/hand.png")));
     MainWindow w;
 
     w.setWindowTitle("Maple Tracker");
