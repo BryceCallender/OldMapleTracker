2dc995426780621b4e47b5d80b2d8880ee569148
Bryce Callender <brycec848@gmail.com>
2022-07-01 15:20:15 -0700

    styling and theme changing

--- MapleDailyTracker/main.cpp
+++ MapleDailyTracker/main.cpp
@@ -1,19 +1,27 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
 
-    w.setWindowTitle("Maple Tracker");
-    w.show();
+    QCoreApplication::setOrganizationName("MapleTracker");
+    QCoreApplication::setApplicationName("MapleDailyTracker");
 
-    QFile styleFile(":/styles/dark_cyan.qss");
+    QSettings settings;
+    QString theme = settings.value("theme", "teal").toString();
+
+    QFile styleFile(QString(":/styles/dark_%1.qss").arg(theme));
     styleFile.open(QFile::ReadOnly);
 
     QString style { styleFile.readAll() };
     a.setStyleSheet(style);
+
+    MainWindow w;
+
+    w.setWindowTitle("Maple Tracker");
+    w.show();
 
     return a.exec();
 }
